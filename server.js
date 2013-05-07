c0c2f55f02426f3ebff77ad97c5cb7deb72cea06
Ivan <iarcuschin@gmail.com>
2013-05-07 18:27:26 -0300

    Initial Commit

--- server.js
+++ server.js
@@ -1,164 +1,79 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+var express = require('express')
+  , io = require('socket.io')
 
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = io.listen(server);
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+// The socket.io server.
+server.listen(8000);
+console.log('socket.io server listening on port 8000');
 
-    //  Scope.
-    var self = this;
+app.listen(3000);
+console.log('application server listening on port 3000');
 
+///////////////////////////////////////////////////////////////////////////////
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+//app.use(express.static(__dirname));
+//app.use(express.bodyParser());
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_INTERNAL_IP;
-        self.port      = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var allowedUserData = ['etoccalino','ivan'];
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_INTERNAL_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+var collected_ = {};
+function getCollected () {
+  var clone = {};
+  for (k in collected_) {
+    if (collected_.hasOwnProperty(k)) {
+      clone[k] = collected_[k];
+    }
+  }
+  return clone;
+}
+function resetCollected (newCollected) {
+  collected_ = newCollected;
+  // for (k in newCollected) {
+  //   if (newCollected.hasOwnProperty(k)) {
+  //     collected_[k] = newCollected[k];
+  //   }
+  // }
+}
 
+// Collect data as it comes
+app.post('/counts', function (req, res) {
+  var user, value, collected = getCollected();
+  for (var i = 0; i < req.body.length; i++) {
+    if (allowedUserData.indexOf(req.body[i].username) != -1) {
+      user = req.body[i].username;
+      value = req.body[i].count;
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+      collected[user] = collected[user] || [];
+      collected[user].push(value);
+    }
+  }
+  // Set the collected values for the app to see.
+  resetCollected(collected);
+  // Answer to key_counter_server.
+  res.send(202, 'Accepted');
+});
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+var PUSH_INTERVAL = 5000;
+setInterval(function () {
+  var collected, value, data = [];
 
+  // Get a copy of the currently collected counts.
+  collected = getCollected();
+  // Reset the collected values.
+  resetCollected({});
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        // Routes for /health, /asciimo and /
-        self.routes['/health'] = function(req, res) {
-            res.send('1');
-        };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+  // Reformat the collected data.
+  for (user in collected) {
+    // Average the collected counts.
+    for (var i = 0, value = 0; i < collected[user].length; i++) {
+      value = value + collected[user][i];
+    }
+    data.push({username: user, count: value / collected[user].length});
+	console.log(JSON.strngify(data);
+  }
+  // Emit to connected clients to update their counts.
+  io.of('/key-count').emit('users update', data);
+}, PUSH_INTERVAL);
