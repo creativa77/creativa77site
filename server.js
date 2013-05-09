e9bf50e0eb943d47583fca61211377f5ae07adac
Ivan <iarcuschin@gmail.com>
2013-05-09 11:23:26 -0300

    Express and Socket.io listening on 8000

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ var express = require('express')
   , io = io.listen(server);
 
 // The socket.io server.
-server.listen(8000);
 console.log('socket.io server listening on port 8000');
 
-//app.listen(3000);
-console.log('application server listening on port 3000');
+app.listen(8000);
+console.log('application server listening on port 8000');
 
 ///////////////////////////////////////////////////////////////////////////////
 
